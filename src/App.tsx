5961b99516ee2a9ca103351969cd630e887939ed
Robert Damaceno <robertdamacenobob@gmail.com>
2022-02-02 10:41:39 -0300

    fix: moving BrowserRouter from /routes/index.tsx to App.tsx to have access to useHistory in AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { FC, Suspense } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import { AuthProvider } from './contexts/AuthContext'
 import { AlertProvider } from './contexts/AlertContext'
 import { ThemeProvider } from './contexts/ThemeContext'
-import { CookiesProvider } from 'react-cookie'
 
 import Router from './routes'
 
 //TODO: ADD ErrorBoundary AND CUSTOM PATHS TO IMPORT
 const App: FC = () => {
     return (
-        <CookiesProvider>
-            <ThemeProvider>
+        <ThemeProvider>
+            <BrowserRouter>
                 <AlertProvider>
                     <AuthProvider>
                         <Suspense fallback={<h1>Carregando...</h1>}>
@@ -19,8 +19,8 @@ const App: FC = () => {
                         </Suspense>
                     </AuthProvider>
                 </AlertProvider>
-            </ThemeProvider>
-        </CookiesProvider>
+            </BrowserRouter>
+        </ThemeProvider>
     )
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,36 +6,34 @@ import paths from './paths'
 
 import RestrictedRoute from './components/RestrictedRoute'
 
-const LazyHomeScreen = lazy(() => import(`../screens/Home`))
-const LazyLoginScreen = lazy(() => import(`../screens/Login`))
-const LazyRegisterScreen = lazy(() => import(`../screens/Register`))
-const LazyNotFoundScreen = lazy(() => import(`../screens/NotFound`))
+const LazyHomeScreen = lazy(() => import(`../pages/Home`))
+const LazyLoginScreen = lazy(() => import(`../pages/Login`))
+const LazyNotFoundScreen = lazy(() => import(`../pages/NotFound`))
+const LazyRegistrationScreen = lazy(() => import(`../pages/Registration`))
 
 const Router: FC = () => {
     return (
-        <BrowserRouter>
-            <Switch>
-                <RestrictedRoute
-                    exact
-                    path={paths.HOME}
-                    authorizedTypeUser="logged"
-                    component={LazyHomeScreen}
-                />
+        <Switch>
+            <RestrictedRoute
+                exact
+                path={paths.HOME}
+                authorizedTypeUser="logged"
+                component={LazyHomeScreen}
+            />
 
-                <RestrictedRoute
-                    path={paths.LOGIN}
-                    authorizedTypeUser="unlogged"
-                    component={LazyLoginScreen}
-                />
-                <RestrictedRoute
-                    path={paths.REGISTER}
-                    authorizedTypeUser="unlogged"
-                    component={LazyRegisterScreen}
-                />
+            <RestrictedRoute
+                path={paths.LOGIN}
+                authorizedTypeUser="unlogged"
+                component={LazyLoginScreen}
+            />
+            <RestrictedRoute
+                path={paths.REGISTRATION}
+                authorizedTypeUser="unlogged"
+                component={LazyRegistrationScreen}
+            />
 
-                <Route component={LazyNotFoundScreen} />
-            </Switch>
-        </BrowserRouter>
+            <Route component={LazyNotFoundScreen} />
+        </Switch>
     )
 }
 
